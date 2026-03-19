f2face61d8e8e5eb6db8f756318a00dbf57a1357
Aline Bierhals <alineevieiraa@hotmail.com>
2026-03-19 08:45:23 -0300

    Automação do contato

---
+++ cypress/e2e/contato.cy.js
@@ -0,0 +1,20 @@
+describe('Funcionalidade=Login', () => {
+
+  it.only('Deve fazer o login com sucesso', () => {
+ 
+  })
+
+  it('Teste 2', () => {
+    cy.visit('http://localhost:3000/index.html')
+  });
+
+  it('Teste 3', () => {
+    cy.visit('http://localhost:3000/index.html')
+  });
+
+  it('Teste 4', () => {
+    cy.visit('http://localhost:3000/index.html')
+  });
+
+
+});
